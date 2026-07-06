008d9cccfd3d484a52091bbe1f569f9575dc6329
ShakiraCasusi <casusishakira28@gmail.com>
2026-07-06 15:24:31 +0800

    fix: add CI/CD deployment workflow for Railway and configure Jest test environment for frontend

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/tests/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 };
